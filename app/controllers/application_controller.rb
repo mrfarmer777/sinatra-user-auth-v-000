a1dd6935790eeda31e80d96c20a5e4dfa06d2c89
Matt Farmer <mrfarmer777@gmail.com>
2018-06-29 17:27:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user=User.find_by(params)
-    if @user=nil
+    if @user==nil
       redirect '/sessions/login'
     else
       session[:id]=@user.id
